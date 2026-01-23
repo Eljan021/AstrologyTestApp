8a5fd1c055a2e6fa7c5189e4594e36c56fe7de0e
Eljan021 <elcan021@bk.ru>
2026-01-23 16:46:52 +0400

    Initial Commit

---
+++ AstrologyTestApp/AstrologyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstrologyTestAppApp.swift
+//  AstrologyTestApp
+//
+//  Created by Eljan on 23.01.26.
+//
+
+import SwiftUI
+
+@main
+struct AstrologyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
